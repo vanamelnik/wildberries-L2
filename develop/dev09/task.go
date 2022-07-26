ae368bd9d9f1e39bee63ecf1d5c5c345b3c148cc
vanamelnik <jmvmiller@gmail.com>
2022-07-26 20:46:19 +0300

    editing dev09

--- develop/dev09/task.go
+++ develop/dev09/task.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
 /*
 === Утилита wget ===
 
@@ -8,6 +16,31 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+type WGet struct {
+	writer io.Writer
+}
 
+func (w WGet) Do(uri string) error {
+	resp, err := http.Get(uri)
+	if err != nil {
+		return fmt.Errorf("http error: %w", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(resp.Body)
+		return fmt.Errorf("%s: %s", resp.Status, string(body))
+	}
+	if w.writer != nil {
+		_, err := io.Copy(w.writer, resp.Body)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func main() {
+	flag.Parse()
+	uri := flag.Arg(0)
+	WGet{os.Stdout}.Do(uri)
 }
